6b4a99a8e1b46d120d5c53344c7f66d31204ecc2
endyd1567 <endyd1567@gmail.com>
2023-04-21 16:14:35 +0900

    2. 두 회사 납품

---
+++ src/main/java/portalService/test2/user/JejuUserDao.java
@@ -0,0 +1,15 @@
+package portalService.test2.user;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static portalService.test2.connection.ConnectionConst.*;
+
+public class JejuUserDao extends UserDao{
+    @Override
+    public Connection getConnection() throws SQLException {
+        Connection con = DriverManager.getConnection(URL_JEJU, USERNAME_JEJU, PASSWORD_JEJU);
+        return con;
+    }
+}
