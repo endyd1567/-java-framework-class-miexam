c8db97e63d48f878f7c2bf3c90f4660b801a6b8a
endyd1567 <endyd1567@gmail.com>
2023-04-21 16:09:58 +0900

    1. insert

--- src/test/java/portalService/test2/user/UserDaoTest.java
+++ src/test/java/portalService/test2/user/UserDaoTest.java
@@ -5,11 +5,12 @@ import org.junit.jupiter.api.Test;
 import java.sql.SQLException;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.in;
 
 class UserDaoTest {
 
     @Test
-    public void get() throws SQLException, SQLException {
+    public void get() throws SQLException {
 
         Long id = 1l;
         String name = "umdu";
@@ -22,7 +23,26 @@ class UserDaoTest {
         assertThat(user.getName()).isEqualTo(name);
         assertThat(user.getPassword()).isEqualTo(password);
 
+    }
 
-    }g
+    @Test
+    public void insert() throws SQLException {
+
+        String name = "엄두용";
+        String password = "2222";
+        User user = new User();
+        user.setName(name);
+        user.setPassword(password);
+
+        UserDao userDao = new UserDao();
+        userDao.insert(user);
+
+        User insertedUser = userDao.findById(user.getId());
+
+        assertThat(insertedUser.getId()).isGreaterThan(1l);
+        assertThat(insertedUser.getId()).isEqualTo(user.getId());
+        assertThat(insertedUser.getName()).isEqualTo(user.getName());
+        assertThat(insertedUser.getPassword()).isEqualTo(user.getPassword());
+    }
 
 }
